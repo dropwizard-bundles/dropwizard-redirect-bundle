01aab88e85cbf19c454d623713ccb21bcacbdf74
Nathaniel Bauernfeind <nate.bauernfeind@gmail.com>
2015-03-24 21:55:16 -0400

    Modify all line length style checks.

--- src/main/java/io/dropwizard/bundles/redirect/HttpsRedirect.java
+++ src/main/java/io/dropwizard/bundles/redirect/HttpsRedirect.java
@@ -6,16 +6,17 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
- * Redirects any non-HTTPS requests to the equivalent HTTPS URL.  This redirect is very simplistic and only relies on
- * what is actually contained in the request URL that the server sees.  It doesn't pay attention to any headers, and
- * specifically not the {@code X-Forwarded-Proto} or {@code X-Forwarded-Port} headers.  This is usually not an issue
- * because the default Dropwizard configuration has {@code http.useForwardedHeaders} set to {@code true}.  When that
- * property is set Dropwizard will configure Jetty to process any of the {@code X-Forwarded-*} headers prior to any
- * servlets being executed.  In general Jersey does the correct things with those headers and will update the request
- * appropriately.
+ * Redirects any non-HTTPS requests to the equivalent HTTPS URL.  This redirect is very simplistic
+ * and only relies on what is actually contained in the request URL that the server sees.  It
+ * doesn't pay attention to any headers, and specifically not the {@code X-Forwarded-Proto} or
+ * {@code X-Forwarded-Port} headers.  This is usually not an issue because the default Dropwizard
+ * configuration has {@code http.useForwardedHeaders} set to {@code true}.  When that property is
+ * set Dropwizard will configure Jetty to process any of the {@code X-Forwarded-*} headers prior to
+ * any servlets being executed.  In general Jersey does the correct things with those headers and
+ * will update the request appropriately.
  * </p>
- * NOTE: If for some reason {@code http.useForwardedHeaders} is not set to {@code true} then this redirect may not
- * work as expected.
+ * NOTE: If for some reason {@code http.useForwardedHeaders} is not set to {@code true} then this
+ * redirect may not work as expected.
  */
 public class HttpsRedirect implements Redirect {
   private final boolean allowPrivateIps;
@@ -25,9 +26,9 @@ public class HttpsRedirect implements Redirect {
   }
 
   /**
-   * @param allowPrivateIps If {@code true} then requests from private ip addresses won't be redirected.  An ip is
-   *                        considered to be private if it is a loopback address, a link local address, or a site
-   *                        local address.
+   * @param allowPrivateIps If {@code true} then requests from private ip addresses won't be
+   *                        redirected.  An ip is considered to be private if it is a loopback
+   *                        address, a link local address, or a site local address.
    */
   public HttpsRedirect(boolean allowPrivateIps) {
     this.allowPrivateIps = allowPrivateIps;
